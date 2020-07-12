eaf9f0a5fedf6c8b0a9b321fde36037c470e16e4
LittleControl <littlecontrol2019@gmail.com>
2020-07-12 16:09:25 +0800

    js相关配置

---
+++ 02/src/js/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable semi */
+
+/* eslint-disable no-console */
+import '../css/index.css';
+
+const myFunc = function myFunc() {
+  return 'LittleControl';
+};
+
+console.log(myFunc());
---
+++ 02/webpack.config.js
@@ -0,0 +1,124 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/built.js',
+    path: resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          // 'style-loader',
+          //将css文件提取为单独的文件
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          //css兼容性处理
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                require('postcss-preset-env')()
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[hash:10]:[ext]',
+              outputPath: 'img',
+              limit: 8 * 1024,
+              esModule: false,
+            },
+          },
+        ],
+      },
+      {
+        exclude: /\.(html|css|js|png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[hash:10]:[ext]',
+              outputPath: 'media',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'eslint-loader',
+            options: {
+              fix: true,
+            },
+          },
+          {
+            //js兼容性处理
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    useBuiltIns: 'usage',
+                    corejs: {
+                      version: 3
+                    },
+                    targets: {
+                      chrome: '60',
+                      firefox: '60',
+                      ie: '9',
+                      safari: '10',
+                      edge: '17'
+                    }
+                  }
+                ]
+              ]
+            }
+          }
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      //压缩html代码
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true
+      }
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/built.css',
+    }),
+    //压缩css
+    new OptimizeCssAssetsWebpackPlugin(),
+  ],
+  //生产环境下会自动压缩js文件
+  mode: 'development',
+  devServer: {
+    contentBase: resolve(__dirname, 'build'),
+    compress: true,
+    port: 8090,
+    open: true,
+  },
+};
